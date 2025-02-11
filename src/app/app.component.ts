c44545449140de6ca33f46c1930ae49bee76387b
Upendra Rai <upendrarai02@gmail.com>
2025-02-11 17:49:12 +0530

    attribute directive practice done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AttributeDirectiveComponent } from './conponents/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AttributeDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
